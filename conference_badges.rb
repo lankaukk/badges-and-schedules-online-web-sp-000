a9794ae2f3be8b33bef87537686de24750a76ed2
McKayla Lankau <lankaukk@yahoo.com>
2020-07-31 02:27:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,12 +21,15 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  list_of_badges_and_room_assignments = []
-  list_of_welcome_messages.each do |w_message|
-    list_of_badges_and_room_assignments << batch_badge_creator(attendees)
-    list_of_badges_and_room_assignments << assign_rooms(attendees)
-  end
-  puts list_of_badges_and_room_assignments
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+  
+  # list_of_badges_and_room_assignments = []
+  # list_of_welcome_messages.each do |w_message|
+  #   list_of_badges_and_room_assignments << batch_badge_creator(attendees)
+  #   list_of_badges_and_room_assignments << assign_rooms(attendees)
+  # end
+  # puts list_of_badges_and_room_assignments
 end
 
 

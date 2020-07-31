55656b1c5366813485798320def24fb3932690d0
McKayla Lankau <lankaukk@yahoo.com>
2020-07-31 02:46:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,6 +23,7 @@ end
 def printer(attendees)
   assign_rooms(attendees).each do |x|
     puts x
+  end
   list_of_badges_and_room_assignments = []
   list_of_welcome_messages.each do |w_message|
     list_of_badges_and_room_assignments << batch_badge_creator(attendees)
